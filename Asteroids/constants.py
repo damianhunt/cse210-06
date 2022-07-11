ca452e7f55210a4289988c6a959dc2ea0fd0511a
damianhunt <dhuntsa@gmail.com>
2022-07-11 23:05:29 +0200

    commit

--- Asteroids/constants.py
+++ Asteroids/constants.py
@@ -21,14 +21,14 @@ FIELD_LEFT = 0
 FIELD_RIGHT = SCREEN_WIDTH
 
 # FONT
-FONT_FILE = "batter/assets/fonts/zorque.otf"
+FONT_FILE = "asteroids/assets/fonts/zorque.otf"
 FONT_SMALL = 32
 FONT_LARGE = 48
 
 # SOUND
-BOUNCE_SOUND = "batter/assets/sounds/boing.wav"
-WELCOME_SOUND = "batter/assets/sounds/start.wav"
-OVER_SOUND = "batter/assets/sounds/over.wav"
+BOUNCE_SOUND = "asteroids/assets/sounds/boing.wav"
+WELCOME_SOUND = "asteroids/assets/sounds/start.wav"
+OVER_SOUND = "asteroids/assets/sounds/over.wav"
 
 # TEXT
 ALIGN_CENTER = 0
@@ -55,7 +55,7 @@ IN_PLAY = 3
 GAME_OVER = 4
 
 # LEVELS
-LEVEL_FILE = "batter/assets/data/level-{:03}.txt"
+LEVEL_FILE = "asteroids/assets/data/level-{:03}.txt"
 BASE_LEVELS = 5
 
 # -------------------------------------------------------------------------------------------------- 
@@ -91,14 +91,14 @@ SCORE_FORMAT = "SCORE: {}"
 
 # BALL
 BALL_GROUP = "balls"
-BALL_IMAGE = "batter/assets/images/000.png"
+BALL_IMAGE = "asteroids/assets/images/000.png"
 BALL_WIDTH = 5
 BALL_HEIGHT = 5
 BALL_VELOCITY = 6
 
 # RACKET
 RACKET_GROUP = "rackets"
-RACKET_IMAGES = [f"batter/assets/images/{n:03}.png" for n in range(100, 103)]
+RACKET_IMAGES = [f"asteroids/assets/images/{n:03}.png" for n in range(100, 103)]
 RACKET_WIDTH = 106
 RACKET_HEIGHT = 28
 RACKET_RATE = 6
@@ -107,10 +107,10 @@ RACKET_VELOCITY = 7
 # BRICK
 BRICK_GROUP = "bricks"
 BRICK_IMAGES = {
-    "b": [f"batter/assets/images/{i:03}.png" for i in range(10,19)],
-    "g": [f"batter/assets/images/{i:03}.png" for i in range(20,29)],
-    "p": [f"batter/assets/images/{i:03}.png" for i in range(30,39)],
-    "y": [f"batter/assets/images/{i:03}.png" for i in range(40,49)]
+    "b": [f"asteroids/assets/images/{i:03}.png" for i in range(10,19)],
+    "g": [f"asteroids/assets/images/{i:03}.png" for i in range(20,29)],
+    "p": [f"asteroids/assets/images/{i:03}.png" for i in range(30,39)],
+    "y": [f"asteroids/assets/images/{i:03}.png" for i in range(40,49)]
 }
 BRICK_WIDTH = 80
 BRICK_HEIGHT = 28
