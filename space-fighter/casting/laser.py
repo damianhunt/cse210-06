5e3944d5ed699bb2c876d363e5ff6a24ddd16d4a
damianhunt <dhuntsa@gmail.com>
2022-07-13 21:12:39 +0200

    commit

--- space-fighter/casting/laser.py
+++ space-fighter/casting/laser.py
@@ -6,26 +6,26 @@ class Laser:
     A projectile fired from the player's and ememy ships.
     '''
     
-#     def __init__(self, x, y, img):
-#         self._x = x
-#         self._y = y
-#         self._img = img
-#         self._mask = pygame.mask.from_surface(self.img)
+    def __init__(self, x, y, img):
+        self._x = x
+        self._y = y
+        self._img = img
+        self._mask = pygame.mask.from_surface(self.img)
 
-#     def draw(self, window):
-#         window.blit(self._img, (self._x, self._y))
+    def draw(self, window):
+        window.blit(self._img, (self._x, self._y))
 
-#     def move(self, vel):
-#         self._y += vel
+    def move(self, vel):
+        self._y += vel
 
-#     def off_screen(self, height):
-#         return not(self._y <= height and self._y >= 0)
+    def off_screen(self, height):
+        return not(self._y <= height and self._y >= 0)
 
-#     def collision(self, obj):
-#         return collide(self, obj)
+    def collision(self, obj):
+        return collide(self, obj)
 
-# def collide(obj1, obj2):
-#     offset_x = obj2.x - obj1.x
-#     offset_y = obj2.y - obj1.y
-#     return obj1._mask.overlap(obj2._mask, (offset_x, offset_y)) != None
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1._mask.overlap(obj2._mask, (offset_x, offset_y)) != None
     
